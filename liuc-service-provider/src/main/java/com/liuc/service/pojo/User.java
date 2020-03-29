5a8896e00c876f6e6f618eb869a724e9fb852192
liuchen6 <1306147839@qq.com>
2020-03-29 19:59:29 +0800

    搭建服务提供方

--- liuc-service-provider/src/main/java/com/liuc/service/pojo/User.java
+++ liuc-service-provider/src/main/java/com/liuc/service/pojo/User.java
@@ -6,6 +6,11 @@ import javax.persistence.*;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    //通用 Mapper 警告信息:
+    // <[EntityColumn{table=user, property='id', column='id', javaType=int, jdbcType=null,
+    // typeHandler=null, id=true, identity=false, blob=false, generator='null', orderBy='null', insertable=true, updatable=true, order=DEFAULT}]>
+    // 使用了基本类型，基本类型在动态 SQL 中由于存在默认值，因此任何时候都不等于 null，建议修改基本类型为对应的包装类型!
     @Column
     private int id;
 
